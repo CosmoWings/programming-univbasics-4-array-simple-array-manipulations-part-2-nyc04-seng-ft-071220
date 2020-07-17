279fb4100784be76f2a96dbbb55eb699bf851d6a
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-17 21:49:42 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+require "pry"
+
+def using_concat(arr1,arr2)
+  combined_arr = arr1.concat(arr2)
+  combined_arr
+end
+
+def using_insert(arr,element)
+  arr.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,interger)
+  array.delete_at(interger)
+end
+
+
+  
